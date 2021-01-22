86c1df619ced0c1783d385ba8534ff3f0538f3d3
camille <spacejamilli20@gmail.com>
2021-01-22 12:59:24 -0600

    submitting

--- README.md
+++ README.md
@@ -23,9 +23,15 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. How would you describe acessibility on the web to someone new to programming?
-2. Talk about 3 different things you can do to ensure your website is accessible. 
+    It is made in a way that it is easier for anyone to navigate.
+2. Talk about 3 different things you can do to ensure your website is accessible.
+    -using semantic html for screen readers
+    -using responsive units so images, font sizes and buttons are scalable on a page
+    -using visuals in association with alerts or important messages
 3. How would you explain the concept of a variable to someone new to programming?
+    A variable is just a placeholder for any data. We can use variables in functions to find or use other needed data.
 4. What is the purpose of using functions in code?
+    Functions use known and unknown data and can implement the data multiple times or whenever necessary.
 
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
--- index.js
+++ index.js
@@ -208,16 +208,17 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
-
+console.log(artists[0].name);
 
 //(2) Bio of the third artist (2nd index) in the array 
 
-
+console.log(artists[2].bio);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+//artists[8].name = 'Vincent Van Gogh';
+//console.log(artists[8].name);
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,7 +229,7 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
+function getArtistByIndex(array, index) {
   /*Your Code Here*/
 }  
 
@@ -242,8 +243,15 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+function get20s(array){
+  let newArray = [];
+
+  for(let i = 0; i < array.length; i++) {
+    if(array[i].years.includes('1900 - 2000')){
+      newArray.push(array[i].name)
+    }
+  }
+  return newArray;
 }
 
 
@@ -257,8 +265,13 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(arr, num){
+ 
+      if (num > -1) {
+        arr.splice(num, 1);
+      }
+  
+  return arr.length;
 }
    
 
@@ -278,8 +291,18 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(array){
+    const newObj =  { 
+      id: 20,
+      name: "Camille Frankkin", 
+      years: "1991 - 0520",
+      genre: "Web Design", 
+      nationality: "American",
+      bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+    } ;
+
+    array.push(newObj);
+    return array;
   }
 
   
@@ -291,8 +314,14 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(array){
+  let newArr = [];
+  for(let i = 0; i < array.length; i++) {
+  if(array[i].paintings > 100){
+      newArr.push(array[i].name);
+    }
+  }
+  return newArr;
 }
 
 
